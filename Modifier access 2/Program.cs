66ed2bf528e7042c106be2592654545998cfeac8
hasher116 <andrei13081996@mail.ru>
2021-07-26 20:32:42 +0300

    Добавьте файлы проекта.

---
+++ Modifier access 2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Modifier_access_2
+{
+    class Program : State
+    {
+        static void Main(string[] args)
+        {
+            Program state1 = new Program();
+
+            //Console.WriteLine(state1.defaultVar);
+            //Console.WriteLine(state1.privateVar);
+            Console.WriteLine(state1.protectedPrivateVar);
+            Console.WriteLine(state1.protectedVar);
+            Console.WriteLine(state1.internalVar);
+            Console.WriteLine(state1.protectedInternalVar);
+            Console.WriteLine(state1.publicVar);
+
+            state1.ProtectedPrivateMethod();
+            state1.ProtectedMethod();
+            state1.InternalMethod();
+            state1.ProtectedInternalMethod();
+            state1.PublicMethod();
+        }
+    }
+}
---
+++ Modifier access 2/State.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modifier_access_2
+{
+    public class State
+    {
+        int defaultVar = 1;
+        private int privateVar = 2;
+        protected private int protectedPrivateVar = 3;
+        protected int protectedVar = 4;
+        internal int internalVar = 5;
+        protected internal int protectedInternalVar = 6;
+        public int publicVar = 7;
+
+        void DefaultMethod()
+        {
+            Console.WriteLine($"defaultVar = {defaultVar}");
+
+        }
+        private void PrivateMethod()
+        {
+            Console.WriteLine($"privateVar = {privateVar}");
+        }
+        protected private void ProtectedPrivateMethod()
+        {
+            Console.WriteLine($"protectedProvateVar = {protectedPrivateVar}");
+        }
+        protected void ProtectedMethod()
+        {
+            Console.WriteLine($"protectedVar = {protectedVar}");
+        }
+        internal void InternalMethod()
+        {
+            Console.WriteLine($"Internal Var = {internalVar}");
+        }
+        protected internal void ProtectedInternalMethod()
+        {
+            Console.WriteLine($"protectedInternalVar = {protectedInternalVar}");
+        }
+        public void PublicMethod()
+        {
+            Console.WriteLine($"publicVar = {publicVar}");
+        }
+
+    }
+}
